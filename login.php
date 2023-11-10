11f3b56b1faa3fc4a9bff9e545c71f7040da4c6c
JOSÉ ERIVALDO DE MEIRELES FILHO <erivaldo.filho@academico.ifrn.edu.br>
2023-11-10 15:31:22 -0300

    atualizar

--- login.php
+++ login.php
@@ -1,222 +1,128 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <!-- Design by foolishdeveloper.com -->
-    <title>Glassmorphism login Form Tutorial in html css</title>
- 
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
-    <!--Stylesheet-->
-    <style media="screen" >
 
-    <link rel="stylesheet" href="style.css"/>
-      *,
-*:before,
-*:after{
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="main.css">
+    
+</head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+body {
+    font-family: 'Inter', sans-serif;
+    margin: 0;
+    padding: 0;
+    color:black ;
+}
+
+.page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    background-color: RGB(169,169,169);
+}
+
+.formLogin {
+    display: flex;
+    flex-direction: column;
+    background-color: #fff;
+    border-radius: 7px;
+    padding: 40px;
+    box-shadow: 10px 10px 40px rgba(0, 0, 0, 0.4);
+    gap: 5px
+}
+
+.areaLogin img {
+    width: 420px;
+}
+
+.formLogin h1 {
     padding: 0;
     margin: 0;
-    box-sizing: border-box;
-}
-body{
-    background-color:#FAF0E6;
-   
-}
-.background{
-    width: 430px;
-    height: 520px;
-    position: absolute;
-    transform: translate(-50%,-50%);
-    left: 50%;
-    top: 50%;
-}
-.shape:first-child{
-    background: linear-gradient(
-        #1845ad,
-        #23a2f6
-    );
-    left: -80px;
-    top: -80px;
-}
-.shape:last-child{
-    background: linear-gradient(
-        to right,
-        #ff512f,
-        #f09819
-    );
-    right: -30px;
-    bottom: -80px;
-}
-form{
-    height: 600px;
-    width: 400px;
-    background-color:#D3D3D3;
-    position: absolute;
-    transform: translate(-50%,-50%);
-    top: 50%;
-    left: 50%;
-    border-radius: 40px;
-    backdrop-filter: blur(10px);
-    border: 2px solid rgba(255,255,255,0.1);
-    box-shadow: 0 0 40px rgba(8,7,16,0.6);
-    padding: 50px 35px;
-}
-form *{
-    font-family: 'Poppins',sans-serif;
-    color:black;
-    letter-spacing: 0.5px;
-    outline:none;
-    border:;
-}
-form h3{
-    font-size: 32px;
     font-weight: 500;
-    line-height: 42px;
-    text-align: center;
+    font-size: 2.3em;
 }
 
-
-
-
-label{
-    display: block;
-    margin-top: 30px;
-    font-size: 16px;
-    font-weight: 500;
-}
-input{
-    display: block;
-    height: 50px;
-    width: 100%;
-    background-color:gray);
-    border-radius: 30px;
-    padding: 0 10px;
-    margin-top: 8px;
+.formLogin p {
+    display: inline-block;
     font-size: 14px;
-    font-weight: 300;
+    color: #666;
+    margin-bottom: 25px;
 }
-::placeholder{
-    color:gray;
+
+.formLogin input {
+    padding: 15px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    margin-bottom: 20px;
+    margin-top: 5px;
+    border-radius: 4px;
+    transition: all linear 160ms;
+    outline: none;
 }
-button{
-    margin-top: 50px;
-    width: 100%;
-    background-color:black;
-    color: white;
-    padding: 15px 0;
-    font-size: 18px;
+
+
+.formLogin input:focus {
+    border: 1px solid #f72585;
+}
+
+.formLogin label {
+    font-size: 14px;
     font-weight: 600;
-    border-radius: 30px;
+}
+
+.formLogin a {
+    display: inline-block;
+    margin-bottom: 20px;
+    font-size: 13px;
+    color: #555;
+    transition: all linear 160ms;
+}
+
+.formLogin a:hover {
+    color: #f72585;
+}
+
+.btn {
+    background-color: #f72585;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 600;
+    border: none !important;
+    transition: all linear 160ms;
     cursor: pointer;
-}
-.social{
-  margin-top: 30px;
-  display: flex;
-}
-.social div{
-  background: red;
-  width: 150px;
-  border-radius: 30px;
-  padding: 5px 10px 10px 5px;
-  background-color:#778899;
-  color: #eaf0fb;
-  text-align: center;
-}
-.social div:hover{
-  background-color: rgba(255,255,255,0.47);
-}
-.social .fb{
-  margin-left: 25px;
-  margin-right:15px;
-  color:#4169E1;
-}
-.social .go{
-  margin-left: 25px;
-  margin-right:15px;
-  color:#DC143C;
+    margin: 0 !important;
+
 }
 
-.social i{
-  margin-right: 4px;
-  color:#4169E1;
+.btn:hover {
+    transform: scale(1.05);
+    background-color: #ff0676;
+
 }
-.social a{
-  margin-right: 4px;
-  color:#DC143C;
-}
-.logar{
-    margin-top:24px;
-    text-align: center;
-}
-
-
-
-
-    </style>
-</head>
-
+</style>
 <body>
-<div class="background">
-<header>
-
-
-   <nav>
-       <ul>
-           <h1>Menu</h1>
-
-
-           <li>
-              
-               <a href=""><i class="bi bi-person-circle"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                   <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                   </svg></i>  Login</a>
-           </li>
-           <li>
-               <a href=""><i class="bi bi-bookmarks"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bookmarks" viewBox="0 0 16 16">
-                   <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4zm2-1a1 1 0 0 0-1 1v10.566l3.723-2.482a.5.5 0 0 1 .554 0L11 14.566V4a1 1 0 0 0-1-1H4z"/>
-                   <path d="M4.268 1H12a1 1 0 0 1 1 1v11.768l.223.148A.5.5 0 0 0 14 13.5V2a2 2 0 0 0-2-2H6a2 2 0 0 0-1.732 1z"/>
-                   </svg></i>  Salvos</a>
-           </li>
-           <li>
-               <a href=""><i class="bi bi-gear-fill"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
-                   <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
-                 </svg></i>   Configurações</a>
-           </li>
-   </nav>
-   </div>
-
-
-   </nav>    
-   </header>
-
-        <div class="shape"></div>
-        <div class="shape"></div>
+    
+    <div class="page">
+        <form method="POST" class="formLogin">
+            <h1>Login</h1>
+            <p>Digite os seus dados de acesso no campo abaixo.</p>
+            <label for="email">E-mail</label>
+            <input type="email" placeholder="Digite seu e-mail" autofocus="true" />
+            <label for="password">Senha</label>
+            <input type="password" placeholder="Digite seu e-mail" />
+            <a href="/">Esqueci minha senha</a>
+            <input type="submit" value="Acessar" class="btn" />
+        </form>
     </div>
-    <form>
-        <h3>LOGIN</h3>
-
-
-
-
-        <label for="username">USUÁRIO</label>
-        <input type="text" placeholder="@email.com" id="username">
-
-
-
-
-        <label for="password">SENHA</label>
-        <input type="password" placeholder="senha" id="password">
-
-
-
-
-        <button>ENTRAR</button>
-        <div class="logar"> <p>LOGAR COM</p></div>
-        <div class="social">
-          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
-          <div class="go"><a class="fab fa-google"></a>  Google</div>
-        </div>
-    </form>
+    
 </body>
 </html>
