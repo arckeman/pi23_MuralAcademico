34998c19ef34824ef1fa564c76d747c39a2675df
arckeman <silva.maria1@escolar.ifrn.edu.br>
2023-11-17 08:16:20 -0300

    Letícia add

--- login.php
+++ login.php
@@ -14,7 +14,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="style.css">  
 </head>
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
@@ -24,11 +23,194 @@ body {
     font-family: 'Inter', sans-serif;
     margin: 0;
     padding: 0;
-    color:black ;
+}
+.menubaixo
+{
+  font-size: 2px;
+  position: relative;
+}
+.menubaixo nav{
+  background-color:white;
+  padding: 6px;
+  width: 600%;
+  height: 900%;
+  top:50px;
+  margin-left:-230px;
+  position:absolute;
+  font-size:15px;
+  display: none;
+  z-index:1;
+  box-shadow: var(--box-shadow);
+}
+.menubaixo nav ul{
+  list-style-type:none;
+  margin-top:20px;
+  margin-left: 30px;
+}
+.menubaixo nav ul li{
+  padding-top: 40px;
+   
+}
+.menubaixo nav a{
+  padding: 5px;
+  display:block;
+  margin-top: -4px;
+}
+.menubaixo nav ul h1{
+  font-size: 30px;
+ 
+}
+.menubaixo nav form{
+    width: 80%;
+    position: relative;
+}    
+.menubaixo label {display:block;}
+.menubaixo input {width: 100%;}
+.menubaixo input,  button {padding: 2px;}
+.menubaixo  button {
+    position: absolute;  
+    top: 0;
+    right: 0;
+  }
+
+input[type="checkbox"]{
+  display:none;
+}
+input[type="checkbox"]:checked~nav{
+  display:block;
+}
+
+.principal
+{
+  width: 75%;
+  align-items: center;
+  margin:0 auto;
+}
+.informacoes
+{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(325px, auto));
+  gap: 1.5rem;
+  margin-top: 40px;
+  height: 100%;
+  border-style: solid;
+  border-color: green;
+  border-bottom-width: 0.5px;
+  border-top-width: 0px;
+  border-right-width: 0px;
+  border-left-width: 0px;
+}
+.imagem
+{
+  padding: 20px;
+  width: 100%;
+}
+.texto
+{
+  width: 100%;
+  padding: 20px;
+ 
+}
+.texto p
+{
+  margin-top:50px;
+  text-align: justify;
+}
+.texto h1
+{
+  text-align: center;
+}
+.texto a{
+  text-align: center;
+}
+
+@media screen and (max-width: 720px) {
+  img {
+    max-width: 100%;
+    object-fit: contain;
+    height: 10em;
+  }
+  .card {
+    max-width: 10em;
+    margin-top: 1em;
+  }
+  #products {
+    grid-template-columns: auto auto;
+    grid-column-gap: 1em;
+  }
+}
+
+.anuncios-container
+{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(325px, auto));
+  gap: 1.5rem;
+  margin-top: 40px;
+  padding: 20px;
+  padding-top: 1px;
+}
+
+.anuncios-container .box
+{
+  position: relative;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 0.5rem;
+  box-shadow: var(--box-shadow);
+}
+
+
+.anuncios-container img
+{
+  width: 100%;
+  height: 250px;
+  object-fit: contain;
+  object-position: center;
+  padding: 20px;
+  background: #f1f1f1;
+  border-radius: 0.5rem;
+}
+
+
+
+.anuncios-container .box content
+{
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+
+.anuncios-container .box .content span
+{
+  padding: 0 1rem;
+  color: var(--bg-color);
+  background: var(--main-color);
+  border-radius: 4px;
+  font-weight: 500;
+}
+
+
+.anuncios-container .box .content a
+{
+  padding: 0 1rem;
+  color: var(--text-color);
+  border: 2px solid var(--main-color);
+  border-radius: 4px;
+  text-transform: uppercase;
+}
+
+.anuncios-container .box .content a:hover
+{
+  background: var(--main-color);
+  color: var(--bg-color);
+  transition: 0.2s all linear;
 }
 header
     {
-    width:%;
+    width: 75%;
     margin: 0 auto;
     top: 0;
     right: 0;
@@ -44,12 +226,10 @@ header
     border-top-width: 0px;
     border-right-width: 0px;
     border-left-width: 0px;
-    }
-
-
+}
 .page {
     display: flex;
-    flex-direction: column;
+    
     align-items: center;
     align-content: center;
     justify-content: center;
@@ -236,15 +416,15 @@ footer p
             <label for="email">E-mail</label>
             <input type="email" placeholder="Digite seu e-mail" autofocus="true" />
             <label for="password">Senha</label>
-            <input type="password" placeholder="Digite seu e-mail" />
+            <input type="password" placeholder="Digite seu e-mail"  placeholder />
             <a href="/">Esqueci minha senha</a>
-            <input type="submit" value="Acessar" class="btn" />
+            <input type="submit" value="Entrar" class="btn" />
         </form>
     </div>
-  
+  <div>
     <footer >
     <p>Copyright © 2023 | Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Norte</p>
     </footer>
-
+</div>
 </body>
 </html>
