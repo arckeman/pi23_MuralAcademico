582f47c0b3fd39fa8809302a2a0e54495ecc99cf
Alexandra Galvão da Silva <alexandra.galvao@academico.ifrn.edu.br>
2023-11-23 16:18:05 -0300

    head

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Stylesheet -->
-    <link rel="stylesheet" style media="screen" href="src/css/style.css"/>
-   
-   
-    <title>anuncios</title>
-</head>
+<?php include 'bases/head.php'?>
+
 <style>
     *
     {
