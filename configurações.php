6416416ffd6404a5854d33912cdd59e70b6717bf
Alexandra Galvão da Silva <alexandra.galvao@academico.ifrn.edu.br>
2023-11-23 16:30:22 -0300

    config

--- configurações.php
+++ configurações.php
@@ -1,14 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="src/css/style.css"/>
-    <title>Cadastro</title>
+<?php include 'bases/head'
 
-<style>
+/*<style>
     *{
     font-family: 'Anton', sans-serif;
     color: black;
@@ -21,9 +16,9 @@
     text-decoration: none;
     background-color:white;
     }
+</style>*/
 
-</style>
-</head>
+?>
 <body>
     
 <!--menu-->
