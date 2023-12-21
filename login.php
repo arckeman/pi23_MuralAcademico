e7dbdeafd32a0b6e079e2603ec830f74dd38dbb2
arckeman <silva.maria1@escolar.ifrn.edu.br>
2023-12-21 09:35:54 -0300

    Marial

--- login.php
+++ login.php
@@ -3,21 +3,6 @@
 <!--head-->
 <?php include 'bases/head.php' ?>
   <style type="text/css">
-  form{
-    display:inline-block;
-    justify-content: center;
-    border:2px solid #000000;
-    background-color:#DCDCDC;
-    margin:6px;
-    margin-left:0;
-    margin-right:80px;
-  }
-  form input{
-    margin: 0 0 20px;
-    padding: 10px 25px;
-    width: 50%;
-    color:black;   
-  }
   .botao{
   text-align: center;
   color:white;
@@ -25,61 +10,72 @@
   font-weight: bold;
   border-radius: 20px;
   margin-left:70px;
-}   
-h2{
-  text-align:center;
+} 
+.informacoes{
+
+  margin-bottom:30px;
+  margin-top:30px;
 }
-.imagem{
-  width:400px;
-  height:380px;
-  margin-top:0;
-  display:inline-block;
-  margin-left:80px;
+.imagem{margin-left:30px;
+ 
 }
-.principal1{
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(325px, auto));
-  gap: 1.0rem;
-  margin-top: 40px;
-  padding: 20px;
-  padding-top: 1px;
-  margin-bottom:250px;
+.texto{
+  border-left:1px solid black;
+
 }
-  </style>
+
+form input{
+  display:inline;
+    text-align: center;
+    padding: 15px 20px;
+    border: 1px solid #eee;
+    border-radius: 6px;
+    background-color:lightgray ;
+ 
+    
+}
+</style>
 </head>
 <body>
 <!--menu-->
 <?php include 'bases/menu.php' ?>
+
 <div class="principal1">
 
-<div class="imagem">
-<img class="img-fluid rounded" src="src/img/fotodoif.jpg" width="75%" height="60%" alt="">
-</div>
+        <div class="informacoes">
 
-<div class="texto">
-<div>
+        <div class="imagem">
+        <img class="img-fluid rounded" src="src/img/fotodoif.jpg" width="75%" height="60%" alt="">
+        </div>
 
-<div class="form">
-<form action="php/bd_autenticação.php" method="post">
-<h2>Login de Autenticação</h2>
-<p>
-    <label for="usuario">Usuário:</label>
-    <input type="text" name="usuario" id="usuario">
-</p>
-<p>
-    <label for="senha">Senha:</label>
-    <input type="password" name="senha" id="senha">
-</p>
-<?php
-         if (isset($_GET['erro'])) {
-         echo "<p class='text-danger'>".$_GET['erro']."</p>";
-         } ?>
-<p><input class="botao" type="submit" value="Entrar"></p>
-</form>
+        <div class="texto">
+       
+        <form action="php/bd_autenticação.php" method="post">
+        <h2>Login de autenticação</h2>
+        <p class="p">
+            <label for="usuario">Usuário:</label>
+            <input type="text" name="usuario" id="usuario">
+        </p>
+        <p class="p">
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha">
+        </p>
+        <?php
+          if (isset($_GET['erro'])) {
+          echo "<p class='text-danger'>".$_GET['erro']."</p>";
+          }
+        ?>
+    
+        <p><input class="botao" type="submit" value="Cadastrar"></p>
+        
+        </form>
+        </div>
+        </div>
+        
+        </div>
 </div>
-</div>
-</div>
-</div>
+        </div>
+        </div>
 <!--rodape-->
 <?php include 'bases/rodape.php' ?>
 </body>
